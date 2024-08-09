c9607f5de5b4f2fa2f7c13c01f97d429de8b8c09
Mathias ravelojaona <core.autocrlf>
2024-08-09 22:05:18 +0200

    fix(front): fix expense add data reload issue

--- app/src/app/expenses/expense.service.spec.ts
+++ app/src/app/expenses/expense.service.spec.ts
@@ -1,23 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 import { ExpenseService } from './expense.service';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
+
+const expenseFactory = () => {
+  return {
+    nature: 'restaurant',
+    amount: 965,
+    comment: 'Enim maioren.',
+    purchasedOn: '2022-05-12',
+    invites: 988,
+  };
+}
+
+const expectedExpenses = {
+  items: [expenseFactory()],
+  count: 1
+};
 
 describe('ExpenseService', () => {
   let service: ExpenseService;
   let httpSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [{ provide: ExpenseService }],
+    httpSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put']);
+    httpSpy.get.and.returnValue(of(expectedExpenses));
+
+      TestBed.configureTestingModule({
+      providers: [{
+        provide: HttpClient,
+        useValue: httpSpy
+      }, ExpenseService ],
     });
     service = TestBed.inject(ExpenseService);
-    httpSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put']);
   });
 
   it('should be created', () => {
@@ -32,39 +47,28 @@ describe('ExpenseService', () => {
     });
   });
 
-  it('should validate sendExpense', () => {
-    let expense = {
-      nature: 'restaurant',
-      amount: 965,
-      comment: 'Enim maioren.',
-      purchasedOn: '2022-05-12',
-      invites: 988,
-    };
+  it('should validate sendExpense', fakeAsync(() => {
+    let expense = expenseFactory();
     (httpSpy.post as jasmine.Spy).and.returnValue(of(expense));
     service.sendExpense(expense).subscribe((x) => {
       expect(x).toEqual(expense as any);
     });
-  });
+  }));
 
-  it('should validate editExpense', () => {
-    let expense = {
-      nature: 'restaurant',
-      amount: 965,
-      comment: 'Enim maioren.',
-      purchasedOn: '2022-05-12',
-      invites: 988,
-    };
-    spyOn(service, 'expenseSignal').and.returnValue({
-      id: 50,
-      nature: 'trip',
-      amount: 965,
-      comment: 'Enim maioren.',
-      purchasedOn: '2022-05-12',
-      distance: 988,
-    });
+  it('should validate editExpense', fakeAsync(() => {
+    let expense = expenseFactory()
+    spyOn(service, 'expenseSignal').and.returnValue(expense);
     (httpSpy.put as jasmine.Spy).and.returnValue(of(expense));
     service.editExpense(expense).subscribe((x) => {
       expect(x).toEqual(expense as any);
     });
-  });
+  }))
+
+  it('should reload data', () => {
+    expect(service.expenses()).toEqual(expectedExpenses.items);
+    expect(httpSpy.get).toHaveBeenCalledTimes(1);
+    service.reload();
+    expect(service.currentPageSignal()).toEqual(1);
+    expect(httpSpy.get).toHaveBeenCalledTimes(2);
+  })
 });
